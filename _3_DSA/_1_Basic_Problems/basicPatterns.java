762ac8ba69fc704d675af5b23a8f6824c525bc30
Akash Halder <halderakash872@gmail.com>
2025-02-02 01:40:58 +0530

    Added new number patterns: Half & Inverted Half Pyramid with Numbers, Flyoid's Triangle, and 1-0 Triangle to basicPatterns.java

--- _3_DSA/_1_Basic_Problems/basicPatterns.java
+++ _3_DSA/_1_Basic_Problems/basicPatterns.java
@@ -71,5 +71,64 @@ public class basicPatterns {
             System.out.println();
         }
 
+
+
+        // pattern 6: Half Pyramid with numbers:
+
+        System.out.println("Half Pyramid with numbers: ");
+        n=5;
+        for(int i=1;i<=n;i++){
+            for(int j = 1; j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+
+
+        // pattern 7: Inverted Half Pyramid with numbers:
+
+        System.out.println("Inverted Half Pyramid with numbers: ");
+        n=5;
+        for(int i=n;i>=1;i--){
+            for(int j = 1; j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+
+
+        // pattern 8: Flyoid's Traingle:
+
+        System.out.println("Flyoid's Traingle: ");
+        n=5;
+        int num = 1; 
+        for(int i=1;i<=n;i++){
+            for(int j = 1; j<=i;j++){
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+
+
+        // pattern 9: 1-0 Traingle:
+
+        System.out.println("Printing 1-0 Traingle: ");
+        n=5;
+        for(int i=1;i<=n;i++){
+            for(int j = 1; j<=i;j++){
+                int sum = i+j;
+                if(sum%2==0){//even
+                    System.out.print("1 ");
+                }else{//odd
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+
+        
     }
 }

fc2b666cd8cb37867e5c8745c41dd2fe85d7b1d6
Akash Halder <halderakash872@gmail.com>
2025-02-05 21:28:43 +0530

    Added practice questions for string manipulation and cumulative length calculation in Strings and StringBuilder classes

--- _3_DSA/_2_Data_Structures/Linear_DataStructures/_1_Arrays_Strings/Strings/_1_Strings.java
+++ _3_DSA/_2_Data_Structures/Linear_DataStructures/_1_Arrays_Strings/Strings/_1_Strings.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class _1_Strings {
+    @SuppressWarnings({"StringEquality", "RedundantStringConstructorCall", "ConvertToTryWithResources"})
     public static void main(String[] args) {
 
         String firstName = "Akash";
@@ -23,6 +26,7 @@ public class _1_Strings {
         } else {
             System.out.println("They are different strings");
         }
+
 
         // DO NOT USE == to check for string equality
         // Gives correct answer here
@@ -59,5 +63,61 @@ public class _1_Strings {
         System.out.println(str1);
 
 
+
+
+        // Practice Questions:
+        // Q1: Take an array of Strings input from the user & find the cumulative(combined) length of all those strings.
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the Array: ");
+        int size = sc.nextInt();
+        String [] namesArr = new String[size];
+        int res = inputArray(namesArr, size, sc);
+        System.out.println("Combined length of those Strings are: "+res);
+
+
+        // Q2. Input a string from the user. Create a new string called ‘result’ in which you will replace the
+        // letter ‘e’ in the original string with letter ‘i’. Ex: original = “eabcdef’ ; result = “iabcdif”
+        System.out.println("Enter a String: ");
+        String org = sc.next();
+        String result = "";
+
+        for(int i=0;i<org.length();i++){
+            if (org.charAt(i)=='e') {
+                result += org.replace('e', 'i');
+                break;                
+            }
+        }
+
+        System.out.println("Result: "+result);
+
+
+        /* Q3. Input an email from the user. You have to create a username from the email by deleting the part that comes after ‘@’. Display that username to the user. 
+        Ex : email = “helloWorld123@gmail.com”; username = “helloWorld123” */ 
+        System.out.print("Enter your Email: ");
+        String email = sc.next();
+        String userName = "";
+
+        for(int i=0;i<email.length();i++){
+            if(email.charAt(i) == '@'){
+                break;
+            }else{
+                userName += email.charAt(i);
+            }
+        }
+        System.out.println("User Name: "+userName);
+
+        sc.close();
+
+    }
+
+    public static int inputArray(String[] arr, int size, Scanner sc) {
+        int toLength = 0;
+        System.out.println("Enter " + size + " strings:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.next(); 
+            toLength+=arr[i].length();
+        }
+        return toLength;
     }
 }
--- _3_DSA/_2_Data_Structures/Linear_DataStructures/_1_Arrays_Strings/Strings/_2_stringBuilder.java
+++ _3_DSA/_2_Data_Structures/Linear_DataStructures/_1_Arrays_Strings/Strings/_2_stringBuilder.java
@@ -1,4 +1,8 @@
+
+import java.util.Scanner;
+
 public class _2_stringBuilder {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
 
         // String Builder is a mutable datatype in Java that points to the same
@@ -50,5 +54,64 @@ public class _2_stringBuilder {
         }
         System.out.println(str);
 
+
+
+
+        // PRACTICE QUESTIONS:
+        // Q1: Find cumulative length of all strings in an array
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the Array: ");
+        int size = sc.nextInt();
+        sc.nextLine(); // Consume newline
+        String[] namesArr = new String[size];
+
+        int res = inputArray(namesArr, size, sc);
+        System.out.println("Combined length of those Strings: " + res);
+
+        // Q2: Replace 'e' with 'i' in a given string using StringBuilder
+        System.out.print("Enter a String: ");
+        String org = sc.nextLine();
+        String result = replaceCharacter(org, 'e', 'i');
+        System.out.println("Result: " + result);
+
+        // Q3: Extract username from email (before '@') using StringBuilder
+        System.out.print("Enter your Email: ");
+        String email = sc.next();
+        String userName = extractUsername(email);
+        System.out.println("User Name: " + userName);
+
+        sc.close(); // Close scanner
+
+    }
+
+    public static int inputArray(String[] arr, int size, Scanner sc) {
+        int totalLength = 0;
+        System.out.println("Enter " + size + " strings:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextLine();
+            totalLength += arr[i].length();
+        }
+        return totalLength;
+    }
+
+    public static String replaceCharacter(String str, char oldChar, char newChar) {
+        StringBuilder sb = new StringBuilder(str);
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == oldChar) {
+                sb.setCharAt(i, newChar);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static String extractUsername(String email) {
+        StringBuilder username = new StringBuilder();
+        for (int i = 0; i < email.length(); i++) {
+            if (email.charAt(i) == '@') {
+                break;
+            }
+            username.append(email.charAt(i)); // Append characters before '@'
+        }
+        return username.toString();
     }
 }
